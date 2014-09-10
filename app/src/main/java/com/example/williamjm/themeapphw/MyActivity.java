b2711224d3ad86d444c8408b81834b723ecf0734
thatsAgoodj1ay <wjmclaugh@msn.com>
2014-09-10 12:07:37 -0500

    ..

--- app/src/main/java/com/example/williamjm/themeapphw/MyActivity.java
+++ app/src/main/java/com/example/williamjm/themeapphw/MyActivity.java
@@ -1,13 +1,12 @@
 package com.example.williamjm.themeapphw;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 
 
 public class MyActivity extends Activity {
@@ -16,6 +15,9 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        MediaPlayer ourSong = MediaPlayer.create(MyActivity.this, R.raw.tgintro);
+        ourSong.start();
+        blinkText();
     }
 
 
@@ -26,35 +28,19 @@ public class MyActivity extends Activity {
         return true;
     }
 
-    public class Blink extends Activity {
-
-        @Override
-        public void onCreate(Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_blink);
-            blinkText();
-        }
-
         private void blinkText() {
             // TODO Auto-generated method stub
             final Handler handler = new Handler();
-            new Thread(new Runnable()
-            {
+            new Thread(new Runnable() {
                 @Override
-                public void run()
-                {
+                public void run() {
                     int timeToBlink = 500;    //in ms
-                    try
-                    {
+                    try {
                         Thread.sleep(timeToBlink);
-                    }
-                    catch (Exception e)
-                    {
+                    } catch (Exception e) {
 
                     }
-                    handler.post(new Runnable()
-                    {
+                    handler.post(new Runnable() {
                         @Override
                         public void run() {
                             TextView txt = (TextView) findViewById(R.id.Header);
@@ -68,19 +54,11 @@ public class MyActivity extends Activity {
                     });
                 }
             }).start();
-
-       /* @Override
-        public boolean onOptionsItemSelected(MenuItem item)
-        {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-            if (id == R.id.action_settings) {
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }*/
         }
     }
-}
+
+
+
+
+
+
